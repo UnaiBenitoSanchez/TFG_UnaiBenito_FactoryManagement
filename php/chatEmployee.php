77fb5c8de58da3ed5aaac86bc61526b494c15c70
UnaiBenitoSanchez <unaibenitos@gmail.com>
2025-05-08 09:13:13 +0200

    ChatEmployee moving out the js that i can move to other file

--- php/chatEmployee.php
+++ php/chatEmployee.php
@@ -53,35 +53,7 @@ if (isset($_SESSION['employee_user'])) {
     <link rel="stylesheet" href="../css/chatEmployee.css">
 
     <!-- JavaScript -->
-    <script>
-        function toggleNavbar() {
-            var navbarNav = document.getElementById('navbarNav');
-            navbarNav.classList.toggle('show');
-
-            document.body.classList.toggle('nav-expanded');
-
-            if (document.body.classList.contains('nav-expanded')) {
-                window.scrollTo({
-                    top: 0,
-                    behavior: 'smooth'
-                });
-            }
-        }
-        document.addEventListener("DOMContentLoaded", function() {
-            const sessionPopup = document.querySelector('.session-popup');
-
-            if (sessionPopup) {
-                setTimeout(function() {
-                    sessionPopup.style.transition = 'opacity 0.5s ease-out';
-                    sessionPopup.style.opacity = '0';
-
-                    setTimeout(function() {
-                        sessionPopup.remove();
-                    }, 500);
-                }, 5000);
-            }
-        });
-    </script>
+    <script src="../js/chatEmployeeConfigs.js"></script>
 
 </head>
 
